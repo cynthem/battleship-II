b4d3e441bdd1625b77cb7f984c485bcfa4b14aee
doozles411 <cindy.phelps01@gmail.com>
2022-10-25 10:05:10 -0700

    Refactor placeShips

--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -25,35 +25,38 @@ export default class Gameboard {
     }
 
     placeShips(ship) {
-        let startChoices;
-        const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
 
         if (ship === this.carrier) {
-            startChoices = [2, 12, 22];
+            const startChoices = [2, 12, 22];
+            const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
             const location = [startPlace, startPlace + 1, startPlace + 2, startPlace + 3, startPlace + 4];
             location.forEach(cell => this.board[cell].shipId = 'carrier');
         }
 
         if (ship === this.battleship) {
-            startChoices = [8, 9, 18, 19, 28, 29, 38, 39, 48, 49, 58, 59, 68, 69];
+            const startChoices = [8, 9, 18, 19, 28, 29, 38, 39, 48, 49, 58, 59, 68, 69];
+            const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
             const location = [startPlace, startPlace + 10, startPlace + 20, startPlace + 30];
             location.forEach(cell => this.board[cell].shipId = 'battleship');
         }
 
         if (ship === this.cruiser) {
-            startChoices = [42, 43, 44, 52, 53, 54, 62, 63, 64];
+            const startChoices = [42, 43, 44, 52, 53, 54, 62, 63, 64];
+            const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
             const location = [startPlace, startPlace + 1, startPlace + 2];
             location.forEach(cell => this.board[cell].shipId = 'cruiser');
         }
 
         if (ship === this.submarine) {
-            startChoices = [0, 10, 20, 30, 40, 50, 60, 70];
+            const startChoices = [0, 10, 20, 30, 40, 50, 60, 70];
+            const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
             const location = [startPlace, startPlace + 10, startPlace + 20];
             location.forEach(cell => this.board[cell].shipId = 'submarine');
         }
 
         if (ship === this.destroyer) {
-            startChoices = [82, 83, 84, 85, 92, 93, 94, 95];
+            const startChoices = [82, 83, 84, 85, 92, 93, 94, 95];
+            const startPlace = startChoices[Math.floor(Math.random() * startChoices.length)];
             const location = [startPlace, startPlace + 1];
             location.forEach(cell => this.board[cell].shipId = 'destroyer');
         }
