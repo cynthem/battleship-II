07492fdaf8e405b3871e421e4167059db4353495
doozles411 <cindy.phelps01@gmail.com>
2022-10-24 18:42:04 -0700

    Configure css and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    ]
+  },
   resolve: {
     alias: {
       jquery: 'jquery/src/jquery'
