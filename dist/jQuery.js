3d6d1855f1693ec744d2a83903a58af8e30973b4
doozles411 <cindy.phelps01@gmail.com>
2022-10-24 18:58:48 -0700

    Initial commit

--- dist/jQuery.js
+++
@@ -1,9 +0,0 @@
-jQuery(function() {
-    $('.content').on('load', () => {
-        $('.new-game').slideDown(1000);
-    });
-
-    $('.player-board').children().on('click', event => {
-        $(event.currentTarget).css('color', '#ff787a');
-    });
-});
