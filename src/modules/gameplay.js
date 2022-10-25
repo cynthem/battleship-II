62c02518ceae574d424fdf8dcc49a9b8551d4059
doozles411 <cindy.phelps01@gmail.com>
2022-10-25 09:57:16 -0700

    Create active class to scale buttons down

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -2,6 +2,7 @@ import Player from "../factories/Player";
 import ComputerMove from "../factories/ComputerMove";
 
 export default function gameplay() {
+    let playerName;
     let userPlayer;
     let computerPlayer;
     let computerMoves;
@@ -11,8 +12,25 @@ export default function gameplay() {
 
     const $playerBoard = $('.player-board > button');
     const $computerBoard = $('.computer-board > button');
+    const $textTop = $('.text-top');
+    const $textBottom = $('.text-bottom');
+    const $textResults = $("<p></p>");
+    const $replayBtn = $('.replay-btn');
+
+    const textPlayerTurn = 'You fire a shot into enemy waters . . .';
+    const textComputerTurn = 'The enemy firest a shot . . .';
+    const textComputerWait = 'The enemy is taking aim . . .';
+    const textMiss = 'and it\'s a miss.';
+    const textHit = 'and it\'s a hit!';
+    const testPlayerSunk = ' You sunk their';
+    const testComputerSunk = ' They sunk your';
+    const textWinTop = `Congratuations ${playerName},`;
+    const textWinBottom = 'You\'re the winner!';
+    const textLoseTop = 'The enemy has won.';
+    const textLoseBottom = 'Better luck next time.';
 
     function beginGame(userName, computerName) {
+        playerName = userName;
         userPlayer = new Player(userName);
         computerPlayer = new Player(computerName);
         computerMoves = new ComputerMove();
@@ -36,9 +54,17 @@ export default function gameplay() {
             cell.style.cursor = 'pointer';
             cell.on('click', userTurn);
         });
-    }
+    };
+
+    function userTurn(event) {
+        
+        $textTop.text('');
+        $textBottom.text('');
+
+    };
 
     return {
-        beginGame
+        beginGame,
+        userTurn
     };
 };
