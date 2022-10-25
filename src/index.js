29b6764dfbf350e3f3253abe29a0d5a648d618c1
doozles411 <cindy.phelps01@gmail.com>
2022-10-24 18:00:53 -0700

    Add jQuery as a plugin

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import jQuery from '../dist/jQuery';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
