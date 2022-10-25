ac00f5bf0736155036977d2f3b44719f1eb72139
doozles411 <cindy.phelps01@gmail.com>
2022-10-25 09:03:03 -0700

    Start beginGame logic

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -1,1 +1,24 @@
-export default function gameplay() {};
+import Player from "../factories/Player";
+import ComputerMove from "../factories/ComputerMove";
+
+export default function gameplay() {
+    let userPlayer;
+    let computerPlayer;
+    let computerMoves;
+    let nextComputerMove;
+    let playerStatus;
+    let computerStatus;
+
+    function beginGame(userName, computerName) {
+        userPlayer = new Player(userName);
+        computerPlayer = new Player(computerName);
+        computerMoves = new ComputerMove();
+
+        nextComputerMove = Math.floor(Math.random() * 100);
+        playerStatus = userPlayer.takeHit(nextComputerMove);
+    }
+
+    return {
+        beginGame
+    };
+};
