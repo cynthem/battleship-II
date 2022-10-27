f6995cd47b1f1fc36160291cd05d4ba6a0ddc871
doozles411 <cindy.phelps01@gmail.com>
2022-10-27 13:13:25 -0700

    Create and pass final test

--- src/tests/computerMove.test.js
+++ src/tests/computerMove.test.js
@@ -12,4 +12,21 @@ describe('ComputerMove functions', () => {
         expect(availableCells).toEqual(100);
     });
 
+    it('receives a status with no ship and returns an index of a different number', () => {
+        const status = {index: 66, shipId: 'none'};
+        const index = nextMove.determineMove(status);
+        expect(index).not.toEqual(66);
+    });
+
+    it('receives a status.index of 0 and returns 10', () => {
+        const status = {index: 0, shipId: 'carrier'};
+        const index = nextMove.determineMove(status);
+        expect(index).toEqual(10);
+    });
+
+    it('receives a status.index of 1 and returns either 0 or 2', () => {
+        const status = {index: 1, shipId: 'submarine'};
+        const index = nextMove.determineMove(status);
+        expect([0, 2]).toContain(index);
+    });
 });
