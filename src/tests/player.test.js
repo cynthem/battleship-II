12c3a3329499d484d3583cd0dee5e3efc06dfbe9
doozles411 <cindy.phelps01@gmail.com>
2022-10-27 12:41:06 -0700

    Create and pass tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -11,4 +11,14 @@ describe('Player functions', () => {
         newPlayer.takeHit(20);
         expect(newPlayer.gameboard.board[20].index).toEqual(20);
     });
+
+    it('takes a hit in an empty space and returns a shipId of "none"', () => {
+        const testHit = newPlayer.takeHit(1);
+        expect(testHit.shipId).toEqual('none');
+    });
+
+    it('takes a hit and returns false for isSunk', () => {
+        const testHit = newPlayer.takeHit(0);
+        expect(testHit.isSunk).toEqual(false);
+    });
 });
