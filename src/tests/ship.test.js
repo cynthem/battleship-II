04d490842b49bbef21f921b18997e35812ffbdb4
doozles411 <cindy.phelps01@gmail.com>
2022-10-27 12:27:42 -0700

    Create and pass tests

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -5,3 +5,24 @@ test('takes a hit', () => {
     battleship.isHit(44);
     expect(battleship.hits).toEqual(1);
 });
+
+test('takes multiple hits', () => {
+    const cruiser = new Ship('cruiser', 3);
+    cruiser.isHit(30);
+    cruiser.isHit(40);
+    expect(cruiser.hits).toEqual(2);
+});
+
+test('determines boat is sunk', () => {
+    const destroyer = new Ship('destroyer', 2);
+    destroyer.isHit(0);
+    destroyer.isHit(1);
+    expect(destroyer.isSunk()).toBe(true);
+});
+
+test('determines boat is not sunk', () => {
+    const submarine = new Ship('submarine', 3);
+    submarine.isHit(0);
+    submarine.isHit(99);
+    expect(submarine.isSunk()).toBe(false);
+});
