769cb6701560c713a71c283e96fbd0b65513ff54
doozles411 <cindy.phelps01@gmail.com>
2022-10-27 12:52:49 -0700

    Pass final test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -11,4 +11,14 @@ describe('Gameboard functions', () => {
         const boardLength = newBoard.board.length;
         expect(boardLength).toEqual(100);
     });
+
+    it('receives an attack on an empty space and returns a shipId of "none"', () => {
+        const status = newBoard.receiveAttack(1);
+        expect(status.shipId).toEqual('none');
+    });
+
+    it('returns false when no ships are sunk', () => {
+        const sunkStatus = newBoard.allSunk();
+        expect(sunkStatus).toEqual(false);
+    });
 });
